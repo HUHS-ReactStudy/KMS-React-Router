55f044fcc1464fba340a8d9138192de449923de5
KIMMINSIK <we_in@naver.com>
2022-05-07 17:39:29 +0900

    router-practice

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+
+
+const NotFound = ()=>{
+
+        return (
+            <div
+            style = {
+                {display : 'flex',
+                alignItems: 'center',
+                jstifyContent: 'center',
+                fontSize:64,
+                position:'absolute',
+                width:'100%',
+                height:'100%'
+                }
+            }>
+                404
+            </div>
+        );
+
+};
+export default NotFound;
